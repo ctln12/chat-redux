0c7b1874c44bee456445426d80947ccc927955fd
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 18:18:22 +0100

    add other divs and classes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,7 +5,8 @@ import MessageList from '../containers/message_list';
 const App = () => {
   return (
     <div className="app">
-      <p>React Chat</p>
+      <div className="logo"><p className="title">LOGO</p></div>
+      <div className="channel-list"><p className="title">Redux Chat</p></div>
       <MessageList />
     </div>
   );
