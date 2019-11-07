c7a4de04b85faedc5dc26db063b92abef17e38d7
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 16:15:36 +0100

    set initial state of messages

--- src/index.jsx
+++ src/index.jsx
@@ -17,7 +17,18 @@ import CurrentUsernameReducer from './reducers/current_username_reducer';
 
 const initialState = {
   // TODO
-  messages: [],
+  messages: [
+    {
+      author: "anonymous92",
+      content: "Hello world!",
+      created_at: "2017-09-26T23:03:16.365Z"
+    },
+    {
+      author: "anonymous77",
+      content: "My name is anonymous77",
+      created_at: "2017-09-26T23:03:21.194Z"
+    }
+  ],
   channels: ['general', 'react', 'paris'],
   selectedChannel: 'general',
   currentUsername: prompt('What is your username?') || `anonymus${Math.floor(10 + (Math.random() * 90))}`
