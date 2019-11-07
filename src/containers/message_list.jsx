ba4cfa3c226a1c9fbe662efdb6c0d49cce62c69a
Cataline Baert <snuf_kin@yahoo.com>
2019-11-07 17:35:07 +0100

    connect container to redux and set initial state of messages to an array of two messages

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,21 +1,32 @@
 import React from 'react';
 
-import connect from 'react-redux';
+import { connect } from 'react-redux';
 
 import Message from '../components/message';
 
 const MessageList = (props) => {
   return (
     <div className="message-list">
-      {props.messages.map(message => <Message message={message} />)}
+      {props.messages.map(message => <Message message={message} key={message.created_at} />)}
     </div>
   );
 };
 
-function mapStateToProps(state) {
+function mapStateToProps() {
   return {
-    messages: state.messages
+    messages: [
+      {
+        author: "anonymous92",
+        content: "Hello world!",
+        created_at: "2017-09-26T23:03:16.365Z"
+      },
+      {
+        author: "anonymous77",
+        content: "My name is anonymous77",
+        created_at: "2017-09-26T23:03:21.194Z"
+      }
+    ]
   };
 }
 
-export default connect(mapStateToProps)(MessageList);
+export default connect(mapStateToProps, null)(MessageList);
