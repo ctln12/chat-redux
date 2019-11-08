1140973b887d6c21c724946e2b2cee6f8f54bdab
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 13:04:43 +0100

    create new container

---
+++ src/containers/message_form.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+// import { setMessages } from '../actions';
+
+class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log(`A name was submitted: ${this.state.value}`);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form className="form-inline" onSubmit={this.handleSubmit}>
+        <input type="text" className="form-control mb-2 mr-sm-2" id="message-form-input" placeholder="Write a message" value={this.state.value} onChange={this.handleChange} style={{ width: '94%' }} />
+        <button type="submit" className="btn btn-danger mb-2">Send</button>
+      </form>
+    );
+  }
+}
+
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators(
+//     { setMessages },
+//     dispatch
+//   );
+// }
+
+// function mapStateToProps(state) {
+//   return {
+//     messages: state.messages
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
+export default MessageForm;
