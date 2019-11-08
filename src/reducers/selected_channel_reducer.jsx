eeb004374e925bbaea80bb0fa1b9aec7e4f2dbac
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 22:48:48 +0100

    replace string by constant

--- src/reducers/selected_channel_reducer.jsx
+++ src/reducers/selected_channel_reducer.jsx
@@ -1,6 +1,9 @@
+import { CHANNEL_SELECTED } from '../actions';
+
+
 export default function(state = null, action) {
   switch (action.type) {
-    case 'CHANNEL_SELECTED':
+    case CHANNEL_SELECTED:
       return action.payload;
     default:
       return state;
