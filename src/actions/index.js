01985f596f4ea53607031237fb67d221afcaf8aa
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 20:46:13 +0100

    refactor set messages

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,12 @@
-// TODO: add and export your own actions
-export function setMessages() {
-  const promise = fetch('https://wagon-chat.herokuapp.com/general/messages')
+const BASE_URL = 'https://wagon-chat.herokuapp.com';
+
+export function setMessages(channel) {
+  const url = `${BASE_URL}/${channel}/messages`;
+  const promise = fetch(url)
     .then(response => response.json());
   return {
     type: 'SET_MESSAGES',
-    payload: promise
+    payload: promise // will be resolved by redux-promise
   };
 }
 
@@ -25,5 +27,18 @@ export function createMessage(channel, author, content) {
   return {
     type: 'CREATE_MESSAGE',
     payload: newMessageList.payload
+  };
+}
+
+export function setChannels(channels) {
+  return {
+    type: 'SET_CHANNELS',
+    payload: channels
+  };
+}
+
+export function setSelectedChannel() {
+  return {
+    type: 'SELECTED_CHANNEL'
   };
 }
