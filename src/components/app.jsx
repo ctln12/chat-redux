761790de24c6c059f4f42bc36ac9d8c62668c0f3
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 20:53:03 +0100

    add new container channel list

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import ChannelList from '../containers/channel_list';
 import MessageList from '../containers/message_list';
 
 const App = () => {
   return (
     <div className="app">
       <div className="logo"><p className="title">LOGO</p></div>
-      <div className="channel-list"><p className="title">Redux Chat</p></div>
+      <ChannelList />
       <MessageList />
     </div>
   );
