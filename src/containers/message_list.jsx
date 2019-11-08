c20258baa4a09520d0903f08c177aa1e3c5ada87
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 12:14:11 +0100

    change component to class, set message in componentWillMount and define dispatch function

--- src/containers/message_list.jsx
+++ src/containers/message_list.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { setMessages } from '../actions';
 
 import Message from '../components/message';
 
-const MessageList = (props) => {
-  return (
-    <div className="message-list">
-      <p className="title">Channel</p>
-      {props.messages.map(message => <Message message={message} key={message.created_at} />)}
-    </div>
-  );
-};
+class MessageList extends Component {
+  componentWillMount() {
+    this.props.setMessages();
+  }
 
-function mapStateToProps() {
+  render() {
+    return (
+      <div className="message-list">
+        <p className="title">Channel</p>
+        {this.props.messages.map(message => <Message message={message} key={message.created_at} />)}
+      </div>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    { setMessages },
+    dispatch
+  );
+}
+
+function mapStateToProps(state) {
   return {
-    messages: [
-      {
-        author: "anonymous92",
-        content: "Hello world!",
-        created_at: "2017-09-26T23:03:16.365Z"
-      },
-      {
-        author: "anonymous77",
-        content: "My name is anonymous77",
-        created_at: "2017-09-26T23:03:21.194Z"
-      }
-    ]
+    messages: state.messages
   };
 }
 
-export default connect(mapStateToProps, null)(MessageList);
+export default connect(mapStateToProps, mapDispatchToProps)(MessageList);
