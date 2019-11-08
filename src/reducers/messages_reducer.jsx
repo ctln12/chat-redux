1eeef357537aa902b333ea4adfa7c64c40d469a6
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 14:58:56 +0100

    add new actions

--- src/reducers/messages_reducer.jsx
+++ src/reducers/messages_reducer.jsx
@@ -1,5 +1,9 @@
 export default function(state = [], action) {
   switch (action.type) {
+    case 'SET_MESSAGES':
+      return action.payload.messages;
+    case 'CREATE_MESSAGE':
+      return action.payload.messages;
     default:
       return state;
   }
