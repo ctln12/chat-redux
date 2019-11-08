cabc2b7b7fd19ebf977bc45641c58b475bbcdca0
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 20:40:04 +0100

    correct create message case and add channel selected case

--- src/reducers/messages_reducer.jsx
+++ src/reducers/messages_reducer.jsx
@@ -2,8 +2,13 @@ export default function(state = [], action) {
   switch (action.type) {
     case 'SET_MESSAGES':
       return action.payload.messages;
-    case 'CREATE_MESSAGE':
-      return action.payload.messages;
+    case 'CREATE_MESSAGE': {
+      const copiedState = state.slice(0);
+      copiedState.push(action.payload);
+      return copiedState;
+    }
+    case 'CHANNEL_SELECTED':
+      return []; // Channel has changed. Clearing view.
     default:
       return state;
   }
