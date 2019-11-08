159ed9963784d495c7d78d019cd93085dfef0dd8
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 22:48:03 +0100

    replace strings by constants

--- src/reducers/messages_reducer.jsx
+++ src/reducers/messages_reducer.jsx
@@ -1,13 +1,15 @@
+import { FETCH_MESSAGES, MESSAGE_POSTED, CHANNEL_SELECTED } from '../actions';
+
 export default function(state = null, action) {
   switch (action.type) {
-    case 'SET_MESSAGES':
+    case FETCH_MESSAGES:
       return action.payload.messages;
-    case 'CREATE_MESSAGE': {
+    case MESSAGE_POSTED: {
       const copiedState = state.slice(0);
       copiedState.push(action.payload);
       return copiedState;
     }
-    case 'CHANNEL_SELECTED':
+    case CHANNEL_SELECTED:
       return []; // Channel has changed. Clearing view.
     default:
       return state;
