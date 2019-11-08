76daccaccabbaa98a3935b946cc20383e9b1a012
Cataline Baert <snuf_kin@yahoo.com>
2019-11-09 00:05:18 +0100

    trim date to keep only time

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const Message = (props) => {
   const { created_at, author, content } = props.message;
+  const time = new Date(created_at).toLocaleTimeString();
   return (
     <div className="message-container" id={props.id}>
-      <p>
-        <span>{author}</span>
-        <small>{created_at}</small>
-      </p>
+      <i>
+        <span>{author} </span>
+        <small>{time}</small>
+      </i>
       <p>{content}</p>
     </div>
   );
