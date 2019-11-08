58068ffbcb847e5670076fcb6decb56be8533a28
Cataline Baert <snuf_kin@yahoo.com>
2019-11-08 20:55:38 +0100

    add id and modify class name

--- src/components/message.jsx
+++ src/components/message.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Message = (props) => {
   return (
-    <div className="message">
+    <div className="message-container" id={props.id}>
       <p>{`${props.message.author} - ${props.message.created_at}`}</p>
       <p>{props.message.content}</p>
     </div>
